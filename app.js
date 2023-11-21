b96efee15d414243349984443230124121ccd1d9
Asio-NOah <asionoah@gmail.com>
2023-11-21 11:27:56 +0300

    21-Nov-11:27

--- app.js
+++ app.js
@@ -7,40 +7,45 @@ const flash = require('express-flash');
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 
-const connectDB = require('./server/config/db');
+const { connectDB, getClient } = require('./server/config/db');
 const session = require('express-session');
 
 const app = express();
-const PORT = 8080 ||  process.env.PORT;
+const PORT = 8080 || process.env.PORT;
 
-//connect database
-connectDB();
+async function startApp() {
+  // Connect database
+  await connectDB();
 
-app.use(express.static('public'));
+  app.use(express.static('public'));
 
-//Templating
-app.use(expressLayout);
-app.set('layout', './layouts/main');
-app.set('view engine', 'ejs');
+  // Templating
+  app.use(expressLayout);
+  app.set('layout', './layouts/main');
+  app.set('view engine', 'ejs');
 
-app.use(methodOverride('_method'));
-app.use('/', require('./server/routes/main'));
-app.use('/', require('./server/routes/admin'));
-app.use(flash());
-app.use(cookieParser());
-app.use(
+  app.use(methodOverride('_method'));
+  app.use('/', require('./server/routes/main'));
+  app.use('/', require('./server/routes/admin'));
+  app.use(flash());
+  app.use(cookieParser());
+
+  app.use(
     session({
       secret: 'keyboardcat',
       resave: false,
-      saveUninitialized: true,
+      saveUninitialized: true,  
       store: MongoStore.create({
         mongoUrl: process.env.MONGODB_URI,
-        // Add the following line to use native MongoDB driver's client
-        client: connectDB.getClient(), // Make sure to adjust this based on your connectDB implementation
+        client: getClient(), // Use the exported getClient function
       }),
     })
   );
- 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-});
+
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
+}
+
+// Call the asynchronous function
+startApp();
